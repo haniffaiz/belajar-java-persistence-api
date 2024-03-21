2c8aac215ed69f540c0348c9e53fe9489aeefb22
Hanif Faiz Hidayat <grey.hanif@gmail.com>
2024-03-21 23:18:04 +0700

    column

--- database.sql
+++ database.sql
@@ -2,3 +2,6 @@ create table customers(
 id varchar(255) not null primary key,
 name varchar(255) not null
 ) engine InnoDB;
+
+alter table customers
+    add column primary_email varchar(150);
---
+++ src/test/java/programmerzamannow/jpa/ColumnTest.java
@@ -0,0 +1,29 @@
+package programmerzamannow.jpa;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import org.junit.jupiter.api.Test;
+import programmerzamannow.jpa.entity.Customer;
+import programmerzamannow.jpa.util.JpaUtil;
+
+public class ColumnTest {
+
+    @Test
+    void column() {
+        EntityManagerFactory entityManagerTest = JpaUtil.getEntityManagerFactory();
+        EntityManager entityManager = entityManagerTest.createEntityManager();
+        EntityTransaction entityTransaction = entityManager.getTransaction();
+        entityTransaction.begin();
+
+        Customer customer = new Customer();
+        customer.setId("1");
+        customer.setName("Hanif");
+        customer.setPrimaryEmail("contoh@example.com");
+
+        entityManager.persist(customer);
+
+        entityTransaction.commit();
+        entityManager.close();
+    }
+}
