1aa7631dbc59c7881fa2349af25d846840ed69dc
Hanif Faiz Hidayat <grey.hanif@gmail.com>
2024-03-23 03:34:38 +0700

    JPA QL : order by clause

--- src/test/java/programmerzamannow/jpa/JpaQueryLanguageTest.java
+++ src/test/java/programmerzamannow/jpa/JpaQueryLanguageTest.java
@@ -97,4 +97,23 @@ public class JpaQueryLanguageTest {
         entityManager.close();
     }
 
+    @Test
+    void orderClause() {
+        EntityManagerFactory entityManagerFactory = JpaUtil.getEntityManagerFactory();
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        EntityTransaction entityTransaction = entityManager.getTransaction();
+        entityTransaction.begin();
+
+        TypedQuery<Brand> query = entityManager.createQuery("select b from Brand b order by b.name desc", Brand.class);
+        List<Brand> brands = query.getResultList();
+
+        for (Brand brand : brands){
+            System.out.println(brand.getId()+" : "+brand.getName());
+        }
+
+
+        entityTransaction.commit();
+        entityManager.close();
+    }
+
 }
